4e50d7910e7a5aafab82aaa691d759bd85007f33
alexandredargelos@gmail.com <alexandredargelos@gmail.com>
2024-10-30 13:36:41 +0000

    int campaign

---
+++ models/intermedite/int_campaigns.sql
@@ -0,0 +1,24 @@
+WITH sub_adwords AS (
+  SELECT * FROM {{ ref('stg_gz_raw_data__adwords') }}
+),
+sub_bing AS (
+  SELECT * FROM {{ ref('stg_gz_raw_data__bing') }}
+),
+sub_criteo AS (
+  SELECT * FROM {{ ref('stg_gz_raw_data__criteo') }}
+),
+sub_facebook AS (
+  SELECT * FROM {{ ref('stg_gz_raw_data__facebook') }}
+)
+
+SELECT *
+FROM sub_adwords
+UNION ALL
+SELECT *
+FROM sub_bing
+UNION ALL
+SELECT *
+FROM sub_criteo
+UNION ALL
+SELECT *
+FROM sub_facebook
